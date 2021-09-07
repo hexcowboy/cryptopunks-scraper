635fbddcbb9030054155ffd4765595dd5c234b89
hexcowboy <hex@cowboy.dev>
2021-09-07 13:13:59 -0500

    update scraper to official punks api

--- scrape.py
+++ scrape.py
@@ -1,68 +1,93 @@
-import requests
 import json
-import re
-from rich import print
+import os
+import signal
+import sys
+from pathlib import Path
+
+import click
+import requests
 from rich.progress import Progress
-from signal import signal, SIGINT
-from sys import exit
 
-url: str = "https://www.larvalabs.com/cryptopunks/details/{id}"
-punks_range: range = range(10_000)
-with open("punks.json") as punks_json_file:
-    punks: dict = json.load(punks_json_file)
+API_URL: str = "https://wrappedpunks.com:3000/api/punks/"
+PUNK_IDS: range = range(10_000)
 
-id_regex = re.compile(
-    '<h1 style="margin-top: 0px; margin-bottom: 5px;">CryptoPunk ([0-9].*)</h1>'
+
+def signal_handler(signal, frame):
+    sys.stdout.write("\b\b\r")
+    print("Gracefully stopping")
+    sys.exit(0)
+
+
+@click.command()
+@click.option(
+    "-o",
+    "--output",
+    "output_file_path",
+    default=None,
+    help="Name of file to send output to (json)",
 )
-species_regex = re.compile(
-    'One of <b>[0-9]*</b> <a href="/cryptopunks/search\?query=.*">(.*)</a> punks\.'
-)
-attrs_regex = re.compile('<a href="/cryptopunks/search\?query=%22.*%22">(.*)</a>')
+def main(output_file_path: str = None) -> dict:
+    # Catch CTRL-C
+    signal.signal(signal.SIGINT, signal_handler)
 
+    punks: dict = dict()
 
-def handler(signal_received, frame):
-    print("Exiting gracefully")
-    with open("punks.json", "w") as file:
-        json.dump(punks, file)
-        print(f"Saved all punks to {file.name}")
-    exit(0)
+    # Load from the output file to resume
+    if output_file_path is not None:
+        output_file = Path(output_file_path)
+        output_file.touch(exist_ok=True)
+        with open(output_file) as file:
+            if os.path.getsize(output_file):
+                punks = json.load(file)
 
-
-def scrape_punks():
     with Progress() as progress:
-        task = progress.add_task("Scraping Punks", total=len(punks_range))
-        for punk in punks_range:
-            if str(punk) in punks:
-                # Punk is already saved in the JSON file
+        task = progress.add_task("Scraping Punks", total=len(PUNK_IDS))
+
+        # for punk_id in PUNK_IDS:
+        for punk_id in PUNK_IDS:
+            if str(punk_id) in punks:
                 continue
 
-            # Get the punk ID from regex searching the page
-            try:
-                response = requests.get(url.format(id=punk))
-                response_text = response.text
-                punk = int(id_regex.findall(response_text)[0])
-            except:
-                break
-            punks[punk] = {}
+            response = requests.get(API_URL + str(punk_id))
+            raw_attributes = json.loads(response.text)
+            punks[punk_id] = dict()
+            punk_type: str = raw_attributes["data"]["attribute"]["Type"]
 
-            # Get the species from regex searching the page
-            species = species_regex.findall(response_text)[0]
-            punks[punk]["species"] = species
+            if punk_type in ["Male", "Female"]:
+                punk_skin = raw_attributes["data"]["attribute"]["Skin"]
+                if "Albino" in punk_skin:
+                    punks[punk_id]["species"] = "Lighter Human"
+                if "Dark" in punk_skin:
+                    punks[punk_id]["species"] = "Darker Human"
+                if "Light" in punk_skin:
+                    punks[punk_id]["species"] = "Light Human"
+                if "Mid" in punk_skin:
+                    punks[punk_id]["species"] = "Dark Human"
+                if punk_type == "Male":
+                    punks[punk_id]["size"] = "Large"
+                elif punk_type == "Female":
+                    punks[punk_id]["size"] = "Petite"
+            else:
+                punks[punk_id]["species"] = str(punk_type).capitalize()
+                punks[punk_id]["size"] = "Large"
 
-            # Get all punk attributes from regex searching the page
-            punks[punk]["attributes"] = []
-            attrs = attrs_regex.findall(response_text)
-            for attr in attrs:
-                punks[punk]["attributes"].append(attr)
+            punks[punk_id]["attributes"] = list()
+            for attribute in range(int(raw_attributes["data"]["attribute"]["Slots"])):
+                punks[punk_id]["attributes"].append(
+                    str(raw_attributes["data"]["attribute"][f"Att{str(attribute + 1)}"])
+                )
 
-            progress.update(task, completed=punk, description=f"Scraping Punk #{punk}")
+            if output_file_path is not None:
+                with open(output_file_path, "w") as file:
+                    json.dump(punks, file)
 
-        handler(SIGINT, scrape_punks)
+            progress.update(
+                task, completed=punk_id, description=f"Scraping Punk #{punk_id}"
+            )
+
+    return punks
 
 
 if __name__ == "__main__":
-    # Tell Python to run the handler() function when SIGINT is recieved
-    signal(SIGINT, handler)
-
-    print("Running. Press CTRL-C to gracefully exit.")
-    scrape_punks()
+    all_punks = main()
+    print(all_punks)
